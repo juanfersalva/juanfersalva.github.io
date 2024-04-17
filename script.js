98f45f1c7fde460649d77fa9b558fa01bdfaf8ca
juanfersalva <juanfersalva@gmail.com>
2024-04-17 13:08:00 +0200

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+scanner.addListener('scan', function (content) {
+    alert("Scanned content: " + content); 
+});
+
+Instascan.Camera.getCameras().then(function (cameras) {
+    if (cameras.length > 0) {
+        scanner.start(cameras[0]); 
+    } else {
+        console.error('No cameras found.');
+    }
+}).catch(function (error) {
+    console.error(error);
+});
