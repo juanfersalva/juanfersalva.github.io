b0b299f7c738067c866547a081063a4abf49450f
juanfersalva <juanfersalva@gmail.com>
2024-04-18 11:39:16 +0200

    nimiq

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const scannerContainer = document.getElementById('scanner-container');
 
 QrScanner.WORKER_PATH = './node_modules/@nimiq/qr-scanner/dist/worker.min.js'; // Adjust path if necessary
 
-const scanner = new QrScanner(scannerContainer, result => {
-    alert('Decoded QR code:', result);
+QrScanner.hasCamera().then(hasCamera => {
+    if (hasCamera) {
+        const scanner = new QrScanner(scannerContainer, result => {
+            alert('Decoded QR code:', result);
+            scanner.stop(); // Stop scanning after the first result
+        });
+        scanner.start(); 
+    } else {
+        console.error('No camera found.');
+    }
 });
-
-scanner.start(); 
